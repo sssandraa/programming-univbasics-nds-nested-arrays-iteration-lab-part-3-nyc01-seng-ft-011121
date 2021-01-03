54fa810ff7b052ead9bb9179dd07f44534c3aa57
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-03 01:19:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ row_index = 0
 while row_index < src.length do
   element_index = 0
   while element_index < src[row_index].length do
-    if src[row_index][element_index].is_a? String 
+    if src[row_index][element_index].is_a? String
       single_string << src[row_index][element_index]
     end
     element_index += 1
